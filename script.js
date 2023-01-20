cc7ddc8f81c46b13867311552ad42191ec713e36
AncaraniSantiago <109321568+AncaraniSantiago@users.noreply.github.com>
2023-01-19 23:18:18 -0300

    Simplificando la elección

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const arrProductos=[
-producto1={id: 1, nombre: "Ficha Macho", categoria: "Accesorios", precio:Number(238),cantidad:Number(0), stock:Number(2000), imgUrl: "https//http2.mlstatic.com/D_NQ_NP_2X_838772-MLA50110819676_052022-F.webp"},
-producto2={id: 2, nombre: "Ficha Hembra", categoria: "Accesorios", precio:Number(262),cantidad:Number(0), stock:Number(2200), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_832554-MLA50110634904_052022-F.webp"},
-producto3={id: 3, nombre: "Contactor", categoria: "Electronica", precio:Number(6578),cantidad:Number(0), stock:Number(10), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_839522-MLA31063051301_062019-F.webp"},
-producto4={id: 4, nombre: "Velador Ferrolux", categoria: "Iluminacion", precio:Number(2870),cantidad:Number(0), stock:Number(30), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_880294-MLA40720397042_022020-F.webp"}
+producto1={id:Number(1), nombre: "Ficha Macho", categoria: "Accesorios",moneda:"$", precio:Number(238),cantidad:Number(0), stock:Number(2000), imgUrl: "https://http2.mlstatic.com/D_NQ_NP_2X_747395-MLA50110782466_052022-F.webp"},
+producto2={id:Number(2), nombre: "Ficha Hembra", categoria: "Accesorios",moneda:"$", precio:Number(262),cantidad:Number(0), stock:Number(2200), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_832554-MLA50110634904_052022-F.webp"},
+producto3={id:Number(3), nombre: "Contactor", categoria: "Electronica",moneda:"$", precio:Number(6578),cantidad:Number(0), stock:Number(10), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_839522-MLA31063051301_062019-F.webp"},
+producto4={id:Number(4), nombre: "Velador Ferrolux", categoria: "Iluminacion",moneda:"$", precio:Number(2870),cantidad:Number(0), stock:Number(30), imgUrl:"https://http2.mlstatic.com/D_NQ_NP_2X_880294-MLA40720397042_022020-F.webp"}
 ];
+arrProductos.forEach(producto => {
+  let tarjetaProducto = document.createElement("div")
+  tarjetaProducto.classList.add("tarjeta")
+  tarjetaProducto.innerHTML=`
+  <h3>${producto.nombre}</h3>
+  <h2>${producto.moneda+producto.precio}</h2>
+  <img src=${producto.imgUrl}/>
+  <button>Comprar</button>
+  `
+  document.body.appendChild(tarjetaProducto)
+})
 const arrCarro=[]
-carro()
+
 function carro() {
   optiOn()
   if (opcion==="1") {
@@ -13,7 +24,6 @@ function carro() {
   }
   else if(opcion==="2"){
     verCarro1()
-    verCarro2()
   }
   else if(opcion==="3"){
     comprarOn()
@@ -22,43 +32,31 @@ function carro() {
 }  
 function optiOn() {
   opcion=prompt("Ingrese el numero correspondiente para acceder\n\n1 - Para ver la lista de productos\n2 - Para ver tu carrito\n3 - Confirmar compra")
+  
 }
 function verListProd() {
   todosNombres=""
   for (const producto of arrProductos) {
-    todosNombres+=(producto.id+" "+producto.nombre+" $"+producto.precio+"\n");
-  }
-  elije=prompt("Elija el producto que desea comprar o ingrese 0 para volver\n\n"+ todosNombres)
-  while(elije!="0") {
-    if (elije==="1") {
-      // producto1.cantidad++;
-      arrCarro.splice(0,1,arrProductos.find((producto) => producto.id == "1", producto1.cantidad++,producto1.stock--))
-      verListProd()
+    todosNombres+=(producto.id+" "+producto.nombre+producto.moneda+producto.precio+"\n");}
+    let elije=Number(prompt("Elija el producto que desea comprar o ingrese 0 para volver\n\n"+ todosNombres)) 
+    while (elije!=0) {
+      for (const producto of arrProductos){ 
+        if(producto.id == elije){ 
+          producto.cantidad++, producto.stock--}  
+        }
+        arrCarro.push(arrProductos.find((producto) => producto.id == elije));
+        verListProd()
+      }
+    carro()  
     }
-    else if(elije==="2"){
-      // producto2.cantidad++;
-      arrCarro.splice(1,1,arrProductos.find((producto) => producto.id=="2",producto2.cantidad++,producto2.stock--))
-      verListProd()
-    }
-    else if(elije==="3"){
-      // producto3.cantidad++;
-      arrCarro.splice(2,1,arrProductos.find((producto) => producto.id=="3",producto3.cantidad++,producto3.stock--))
-      verListProd()
-    }
-    else if(elije==="4"){
-      // producto4.cantidad++;
-      arrCarro.splcie(3,1,arrProductos.find((producto) => producto.id=="4",producto4.cantidad++,producto4.stock--))
-      verListProd()
-    }
-    else{
-      carro()
-    }
-  }
-  carro()
-}
+
 function verCarro1() {
+  const unicos = arrCarro.filter((valor, indice) => {
+    return arrCarro.indexOf(valor) === indice;
+  }
+);
   let todosCarro=""
-  arrCarro.forEach (producto =>  {
+  unicos.forEach (producto =>  {
   todosCarro+=("Nombre: "+producto.nombre+" $ "+producto.precio+" Cantidad: "+producto.cantidad+"\n");
     }
   )
@@ -66,13 +64,10 @@ function verCarro1() {
   alert(todosCarro)
   carro()
 }
-function verCarro2() {
-}
 function comprarOn() {
   let tot=Number()
   arrCarro.forEach(producto => {
   tot += (producto.cantidad*producto.precio)
   })
 alert("Gracias por su compra\n\nEl total a abonar es de: $ "+tot)
-carro()
 }
